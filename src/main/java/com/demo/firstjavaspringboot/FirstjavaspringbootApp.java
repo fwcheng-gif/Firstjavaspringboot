26bc390338ff1154ce157fadf3e40c4ff558f339
WenCheng Fan <fanwenchen10@gmail.com>
2026-03-25 16:30:27 +0800

    Initial commit

---
+++ src/main/java/com/demo/firstjavaspringboot/FirstjavaspringbootApp.java
@@ -0,0 +1,12 @@
+package com.demo.firstjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjavaspringbootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstjavaspringbootApp.class, args);
+    }
+}
---
+++ src/test/java/com/demo/firstjavaspringboot/FirstjavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.firstjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjavaspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
